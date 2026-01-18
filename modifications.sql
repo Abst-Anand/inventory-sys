ff64bda146c3a38ac93030635973969bfff43e5b
Anand Raj <cse.20bcse33@silicon.ac.in>
2026-01-19 00:19:49 +0530

    added db modification file

---
+++ modifications.sql
@@ -0,0 +1,7 @@
+
+alter table bill_items modify price int;
+alter table bills modify total_amount int;
+alter table products modify costprice int;
+alter table products modify sellprice int;
+alter table purchaseinfo modify totalcost int;
+alter table salesinfo modify revenue int;
